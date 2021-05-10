586788b38568480ad8281f23a9dbb0b03bf8506d
Enterprise09 <goldenstaytes@gmail.com>
2021-05-10 15:44:10 +0900

    Method Overloading

---
+++ Tenth_project/Tenth_5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+
+namespace Tenth_5
+{
+    class Program
+    {
+        class MyMath
+        {
+            //Method Overloading
+            public static int Abs(int x)
+            {
+                if (x < 0) { return -x; }
+                else { return x; }
+            }
+
+            public static double Abs(double x)
+            {
+                if (x < 0) { return -x; }
+                else { return x; }
+            }
+
+            public static long Abs(long x)
+            {
+                if (x < 0) { return -x; }
+                else { return x; }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //int
+            WriteLine(MyMath.Abs(52));
+            WriteLine(MyMath.Abs(-273));
+
+            //double
+            WriteLine(MyMath.Abs(52.273));
+            WriteLine(MyMath.Abs(-30.103));
+
+            //long
+            WriteLine(MyMath.Abs(21474836470));
+            WriteLine(MyMath.Abs(-21474836470));
+
+        }
+    }
+}
