8f2e6c1a0c81cf20479d75be69c0dfc43a67aae1
Enterprise09 <goldenstaytes@gmail.com>
2021-05-17 15:49:20 +0900

    private Field _ wrong Example

--- Eleventh-2/Boxes/BoxB.cs
+++ Eleventh-2/Boxes/BoxB.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Boxes
 {
     class BoxB
     {
+        //캡슐화 : 변수 width 와 height 를 건드리지 못하도록 BoxA에서 수정
+        private int width;
+        private int height;
+
+        public BoxB(int width, int height)
+        {
+            if(width > 0 && height > 0)
+            {
+                this.width = width;
+                this.height = height;
+            }
+            else
+            {
+                WriteLine("너비와 높이는 자연수로 초기화해 주세요!");   
+            }
+        }
+
+        public int Area()
+        {
+            return width * height;
+        }
     }
 }
--- Eleventh-2/Boxes/Program.cs
+++ Eleventh-2/Boxes/Program.cs
@@ -6,9 +6,15 @@ namespace Boxes
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            /*
             BoxA boxA = new BoxA(10, 10);
             WriteLine(boxA.Area());
+            */
+
+            BoxB boxB = new BoxB(10, 10);
+            WriteLine(boxB.Area());
+
         }
     }
 }
