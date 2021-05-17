da556c4317060add123b9416e650ce03c77ec0c7
Enterprise09 <goldenstaytes@gmail.com>
2021-05-17 14:32:04 +0900

    Week11 _ Create Solution Eleventh-1

---
+++ Eleventh-1/Property_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Property_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
