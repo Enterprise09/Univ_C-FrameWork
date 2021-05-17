2133edc8782012b56e814ad5d683114169db6006
Enterprise09 <goldenstaytes@gmail.com>
2021-05-17 15:16:53 +0900

    Create Property_3 Project

---
+++ Eleventh-1/Property_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Property_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
