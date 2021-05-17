4ea3666627d2e372219fc89de744592e9069c11f
Enterprise09 <goldenstaytes@gmail.com>
2021-05-17 16:35:30 +0900

    Close BoxD _ Property const

--- Eleventh-2/Boxes/Program.cs
+++ Eleventh-2/Boxes/Program.cs
@@ -6,14 +6,23 @@ namespace Boxes
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
             /*
             BoxA boxA = new BoxA(10, 10);
             WriteLine(boxA.Area());
-            */
+            
 
             BoxB boxB = new BoxB(10, 10);
             WriteLine(boxB.Area());
+            
+
+            BoxC boxC = new BoxC(10, 10);
+            WriteLine(boxC.Area());
+            */
+
+            BoxD boxD = new BoxD(10, 10);
+            WriteLine(boxD.Area());
+
 
         }
     }
