7c245f1e47c085d2e936da459ce28bfb03b8990a
Enterprise09 <goldenstaytes@gmail.com>
2021-05-24 16:08:15 +0900

    Create 5th Project

---
+++ Twelfth-1/ShadowAndHide/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShadowAndHide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
