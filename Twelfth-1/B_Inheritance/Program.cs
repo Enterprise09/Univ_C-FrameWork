db57c5d68ea3a025e79b83c6cbb63570b9645f55
Enterprise09 <goldenstaytes@gmail.com>
2021-05-24 14:55:59 +0900

    extends(:) Example

--- Twelfth-1/B_Inheritance/Program.cs
+++ Twelfth-1/B_Inheritance/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using static System.Console;
 
 namespace B_Inheritance
 {
+    class Animal
+    {
+        public int Age { get; set; }
+        public Animal() { this.Age = 0; }
+        public void Eat() { WriteLine("Eat"); }
+        public void Sleep() { WriteLine("Sleep"); }
+
+    }
+
+    class Dog : Animal
+    {
+        
+        public String Color { get; set; }
+        public void Bark() { WriteLine("Bark"); }
+    }
+
+    class Cat : Animal
+    {
+        public void Meow() { WriteLine("cat's Bark"); }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
 
+            WriteLine("Dogs");
+            foreach (var item in Dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+                WriteLine("Age : " + item.Age);
+                WriteLine("---------------------");
+            }
+
+            WriteLine("Cats");
+            foreach (var item in Cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+                WriteLine("Age : " + item.Age);
+                WriteLine("---------------------");
+            }
         }
     }
 }
