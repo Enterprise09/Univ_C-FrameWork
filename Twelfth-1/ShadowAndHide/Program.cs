62fb32df236bb57b0fe4cad90a9df84f794ec441
Enterprise09 <goldenstaytes@gmail.com>
2021-05-24 16:28:31 +0900

    Shadowing

--- Twelfth-1/ShadowAndHide/Program.cs
+++ Twelfth-1/ShadowAndHide/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using static System.Console;
 
 namespace ShadowAndHide
 {
+    public class Parent
+    {
+        public int variable = 273;
+
+        public Parent()
+        {
+            WriteLine("Parent ");
+        }
+
+        public void Method()
+        {
+            WriteLine("Parent Method");
+        }
+    }
+
+    public class Child : Parent
+    {
+        public int variable = 2;
+
+        public Child() : base()
+        {
+            WriteLine("Child");
+        }
+
+        public void Method()
+        {
+            WriteLine("Child Method");
+        }
+    }
     class Program
     {
+        public static int number = 10;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = 20;
+            WriteLine(number);
+            WriteLine();
+
+            Child childA = new Child();
+            WriteLine(childA.variable);
+            childA.Method();
+            WriteLine();
+
+            Child childB = new Child();
+            WriteLine(((Parent)childB).variable);
+            ((Parent)childB).Method();
+            
         }
     }
 }
