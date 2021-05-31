cf1b206f06482cb77d2a1c292a9aab06d76dadf6
Enterprise09 <goldenstaytes@gmail.com>
2021-05-31 16:13:06 +0900

    Try and Catch Exception Process

--- Thirteenth-2/Exception_2/Program.cs
+++ Thirteenth-2/Exception_2/Program.cs
@@ -7,13 +7,32 @@ namespace Exception_2
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3 };
-            for(int i = 0; i < 3; i++)
+            try
             {
-                WriteLine(arr[i]);
+                int[] arr = { 1, 2, 3 };
+                for (int i = 0; i < 5; i++)
+                {
+                    WriteLine(arr[i]);
+                }
+            }
+            catch
+            {
+                WriteLine("종료");
             }
 
-            WriteLine("종료");
+            //시험문제 테스트
+            /*for(int k=1; k<=5; k++)
+            {
+                for(int i=5-k; i>=0; i--)
+                {
+                    Write(" ");
+                }
+                for(int j=1; j<=k*2-1; j++)
+                {
+                    Write("*");
+                }
+                WriteLine("");
+            }*/
         }
     }
 }
