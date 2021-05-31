9c988d4bcb60fea55ca261df285af0901c9bf65f
Enterprise09 <goldenstaytes@gmail.com>
2021-05-31 15:42:14 +0900

    Add Comment

--- Thirteenth-2/Exception_1/Program.cs
+++ Thirteenth-2/Exception_1/Program.cs
@@ -12,6 +12,7 @@ namespace Exception_1
             int input = int.Parse(ReadLine());
             WriteLine("입력한 위치의 값은 '" + array[input] + "'입니다.");*/
 
+            //Basic Exception Process
             string[] array = { "가", "나" };
             WriteLine("숫자를 입력해주세요 : ");
             int input = int.Parse(ReadLine());
