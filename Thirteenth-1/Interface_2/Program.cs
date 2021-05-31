5479ac85e8ceb57452694f4197d64154a4da4d91
Enterprise09 <goldenstaytes@gmail.com>
2021-05-31 15:27:15 +0900

    Custom Interface

--- Thirteenth-1/Interface_2/Program.cs
+++ Thirteenth-1/Interface_2/Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using static System.Console;
 
 namespace Interface_2
 {
     class Program
     {
+        class TestClass : IBasic    // (2)Interface
+        {
+            // (3)Implement Interface
+            public int TestProperty { 
+                get => throw new NotImplementedException(); 
+                set => throw new NotImplementedException();
+            }
+
+            public int TestInstanceMethod()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            // (4)다형성 구현
+            IBasic basic = new TestClass();
             
         }
     }
