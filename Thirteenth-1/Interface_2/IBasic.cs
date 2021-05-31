8f76e3b950406bea3513f623a19bfa829a7371ef
Enterprise09 <goldenstaytes@gmail.com>
2021-05-31 15:16:29 +0900

    Create Interface

---
+++ Thirteenth-1/Interface_2/IBasic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_2
+{
+    // (1) Create Interface
+    interface IBasic
+    {
+        int TestInstanceMethod();
+        int TestProperty { get; set; }
+    }
+}
