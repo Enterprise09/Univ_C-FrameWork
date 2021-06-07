7d79044061d9b49df09ee7d18d6301cd591d0911
Enterprise09 <goldenstaytes@gmail.com>
2021-06-07 17:03:03 +0900

    Thread Tesing

---
+++ Fourteenth/Thread_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace Thread_1
+{
+    class Program
+    {
+        static void ThreadBody()
+        {
+            for (int i = 0; i <= 15; i++)
+            {
+                WriteLine(DateTime.Now.Second + " : " + i);
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart ts = new ThreadStart(ThreadBody);
+            Thread t = new Thread(ts);
+
+            WriteLine("*** Start of Main");
+            t.Start();
+            WriteLine("*** End of Main");
+        }
+    }
+}
