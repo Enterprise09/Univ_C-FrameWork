f9fd47ddef253099a0f76de9c30a20c86ef478c5
Enterprise09 <goldenstaytes@gmail.com>
2021-06-07 16:47:18 +0900

    Complete Lambda

--- Fourteenth/Lambda/Program.cs
+++ Fourteenth/Lambda/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using static System.Console;
 
 namespace Lambda
 {
     class Program
     {
+        delegate int Calculate(int a, int b);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Calculate calc = (int a, int b) => a + b;*/
+            Calculate calc = (a, b) => a + b;   //형식유추형 Lambda
+            WriteLine($"{3} + {4} : {calc(3, 4)}");
         }
     }
 }
