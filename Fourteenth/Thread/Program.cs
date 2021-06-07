b5cd74cb770c920d5b5009a9e8cd9db0d490cde1
Enterprise09 <goldenstaytes@gmail.com>
2021-06-07 16:52:00 +0900

    Delete Thread Project for Rename

---
+++ Fourteenth/Thread/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Thread
+{
+    class Program
+    {
+        static void ThreadBody()
+        {
+            for(int i = 0; i<=15; i++)
+            {
+                WriteLine(DateTime.Now.Second + " : " + i);
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
