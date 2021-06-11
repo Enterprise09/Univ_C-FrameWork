e8add684fbbb346d2804d7d8ece25865b9ed71e8
Enterprise09 <goldenstaytes@gmail.com>
2021-06-11 18:15:28 +0900

    Test for List

--- FinalTest_prepare/HidingTest/Program.cs
+++ FinalTest_prepare/HidingTest/Program.cs
@@ -1,30 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace HidingTest
 {
-    class BaseState
+    class Animal
     {
-        public virtual void met()
+        public virtual void Eat()
         {
-            WriteLine("Parent Met");
+            WriteLine("냠냠 먹습니다.");
         }
     }
 
-    class ChildState : BaseState
+    class Dog : Animal
     {
-        public override void met()
+        public new void Eat()
         {
-            WriteLine("Child Met");
+            WriteLine("강아지 사료를 먹습니다.");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void Eat()
+        {
+            WriteLine("고양이 사료를 먹습니다.");
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            ChildState childState = new ChildState();
-            childState.met();
-            ((BaseState)childState).met();
+            List<Animal> Animals = new List<Animal>()
+            {
+                new Dog(), new Cat(), new Cat(), new Dog(),
+                new Dog(), new Cat(), new Dog(), new Dog()
+            };
+
+            foreach (var item in Animals)
+            {
+                item.Eat();
+            }
 
         }
     }
