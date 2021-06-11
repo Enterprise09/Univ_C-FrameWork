2b70312d7217bdb26cb2e173736cab875fc8dc01
Enterprise09 <goldenstaytes@gmail.com>
2021-06-11 20:02:49 +0900

    Create IDisposable Test Project

---
+++ FinalTest_prepare/IDisposableTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace IDisposableTest
+{
+    class Program
+    {
+        class Durmmy : IDisposable
+        {
+            public void Dispose()
+            {
+                WriteLine("Dispose()메소드 호출됨");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Durmmy durmmy = new Durmmy();
+            durmmy.Dispose();
+
+        }
+    }
+}
