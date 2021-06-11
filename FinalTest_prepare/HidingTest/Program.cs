a02ce20c6df9c7ac0533205a488630ad55c63165
Enterprise09 <goldenstaytes@gmail.com>
2021-06-11 18:08:54 +0900

    Compare to override and hiding

--- FinalTest_prepare/HidingTest/Program.cs
+++ FinalTest_prepare/HidingTest/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using static System.Console;
 
 namespace HidingTest
 {
+    class BaseState
+    {
+        public virtual void met()
+        {
+            WriteLine("Parent Met");
+        }
+    }
+
+    class ChildState : BaseState
+    {
+        public override void met()
+        {
+            WriteLine("Child Met");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ChildState childState = new ChildState();
+            childState.met();
+            ((BaseState)childState).met();
+
         }
     }
 }
