ac6fb91ec132deae4ddbee192c816250d30d8e67
Enterprise09 <goldenstaytes@gmail.com>
2021-06-11 20:14:52 +0900

    Test for Interface

---
+++ FinalTest_prepare/CustomInterface/ILogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CustomInterface
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+    }
+
+    interface IFormattaableLogger : ILogger
+    {
+        void WriteLog(string format, params Object[] arg);
+    }
+
+    class ConsoleLogger2 : IFormattaableLogger
+    {
+        public void WriteLog(string format, params object[] arg)
+        {
+            String message = String.Format(format, arg);
+            WriteLine($"{DateTime.Now.ToLocalTime()}, {message}");
+        }
+
+        public void WriteLog(string message)
+        {
+            WriteLine($"{DateTime.Now.ToLocalTime()}, {message}");
+        }
+    }
+}
--- FinalTest_prepare/CustomInterface/Program.cs
+++ FinalTest_prepare/CustomInterface/Program.cs
@@ -6,7 +6,9 @@ namespace CustomInterface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IFormattaableLogger logger = new ConsoleLogger2();
+            logger.WriteLog("The world is not flat");
+            logger.WriteLog("{0} + {1} + {2}", 1, 1, 2);
         }
     }
 }
