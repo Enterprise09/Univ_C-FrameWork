3e6467e8581e18146eed43cf22fb029410fc3cf0
Enterprise09 <goldenstaytes@gmail.com>
2021-06-11 18:03:02 +0900

    Create Final Test Solution

---
+++ FinalTest_prepare/HidingTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HidingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
