d93f16b648ea3c94dc9a7aac5527187ec1088df2
Enterprise09 <goldenstaytes@gmail.com>
2021-06-12 18:01:06 +0900

    Create Lambda Project And Practice

---
+++ FinalTest_prepare/LambdaExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace LambdaExample
+{
+    
+    class Program
+    {
+        delegate int Calc(int a, int b);
+        delegate void Dosomething();
+        static void Main(string[] args)
+        {
+            Calc calc = (a, b) => a + b;
+            Dosomething dosomething = () => {
+                WriteLine("Dosomthing");
+            };
+            WriteLine(calc(3, 4));
+            dosomething();
+        }
+    }
+}
