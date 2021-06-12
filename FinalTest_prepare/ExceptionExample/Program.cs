64d413c636dfad93ee5e7dbd6c3d26fbcdaf8407
Enterprise09 <goldenstaytes@gmail.com>
2021-06-12 13:54:57 +0900

    Create Exception Example Project

---
+++ FinalTest_prepare/ExceptionExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
