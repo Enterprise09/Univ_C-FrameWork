93e108c93ae2bcbff61d155f5fd81035074925cc
Enterprise09 <goldenstaytes@gmail.com>
2021-06-12 17:00:33 +0900

    Registed to Handler

--- FinalTest_prepare/EventExample/Program.cs
+++ FinalTest_prepare/EventExample/Program.cs
@@ -3,11 +3,34 @@ using static System.Console;
 
 namespace EventExample
 {
+    public delegate void MyEventHandler();
+    class Button
+    {
+        public event MyEventHandler Push;
+        public void OnPush()
+        {
+            if (Push != null)
+            {
+                Push();
+            }
+        }
+    }
+
+    class MyEventHandlerClass
+    {
+        public void MyMethod()
+        {
+            WriteLine("이벤트 발생 : MyMethod()실행됨");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Button button = new Button();
+            MyEventHandlerClass obj = new MyEventHandlerClass();
+            button.Push += new MyEventHandler(obj.MyMethod);
+            button.OnPush();
         }
     }
 }
