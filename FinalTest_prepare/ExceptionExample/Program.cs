ddc9a1f87f2f3642fe31d13e1a59e5f1c45cfa6e
Enterprise09 <goldenstaytes@gmail.com>
2021-06-12 14:05:01 +0900

    Review try-catch-finally

--- FinalTest_prepare/ExceptionExample/Program.cs
+++ FinalTest_prepare/ExceptionExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace ExceptionExample
 {
@@ -6,7 +7,24 @@ namespace ExceptionExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                WriteLine("숫자 입력 : ");
+                string input = ReadLine();
+
+                int index = int.Parse(input);
+                WriteLine("입력 숫자 : " + index);
+            }
+            catch (Exception e)
+            {
+                WriteLine($"예외가 발생했습니다. : {e.Message}");
+            }
+            finally
+            {
+                WriteLine("프로그램 종료");
+            }
+
+
         }
     }
 }
