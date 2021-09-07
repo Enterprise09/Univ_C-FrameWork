611ad21b52a99114ad79262d334a711aa86d55d3
Enterprise09 <goldenstaytes@gmail.com>
2021-09-07 14:50:15 +0900

    finished ListView Project

---
+++ B-2/B_ListView/Form2.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace B_ListView
+{
+    partial class Form2
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvView = new System.Windows.Forms.ListView();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblWork = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtWork = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.chName = new System.Windows.Forms.ColumnHeader();
+            this.chAge = new System.Windows.Forms.ColumnHeader();
+            this.chWork = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // lvView
+            // 
+            this.lvView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chAge,
+            this.chWork});
+            this.lvView.FullRowSelect = true;
+            this.lvView.GridLines = true;
+            this.lvView.HideSelection = false;
+            this.lvView.Location = new System.Drawing.Point(12, 12);
+            this.lvView.Name = "lvView";
+            this.lvView.Size = new System.Drawing.Size(214, 274);
+            this.lvView.TabIndex = 0;
+            this.lvView.UseCompatibleStateImageBehavior = false;
+            this.lvView.Click += new System.EventHandler(this.lvView_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblName.Location = new System.Drawing.Point(276, 35);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(42, 21);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "이름";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAge.Location = new System.Drawing.Point(276, 77);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(42, 21);
+            this.lblAge.TabIndex = 2;
+            this.lblAge.Text = "나이";
+            // 
+            // lblWork
+            // 
+            this.lblWork.AutoSize = true;
+            this.lblWork.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblWork.Location = new System.Drawing.Point(276, 119);
+            this.lblWork.Name = "lblWork";
+            this.lblWork.Size = new System.Drawing.Size(42, 21);
+            this.lblWork.TabIndex = 3;
+            this.lblWork.Text = "직업";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(324, 33);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(126, 23);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(324, 75);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(126, 23);
+            this.txtAge.TabIndex = 5;
+            // 
+            // txtWork
+            // 
+            this.txtWork.Location = new System.Drawing.Point(324, 117);
+            this.txtWork.Name = "txtWork";
+            this.txtWork.Size = new System.Drawing.Size(126, 23);
+            this.txtWork.TabIndex = 6;
+            this.txtWork.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtWork_KeyPress);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.Location = new System.Drawing.Point(276, 159);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(174, 27);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // chName
+            // 
+            this.chName.Width = 100;
+            // 
+            // chAge
+            // 
+            this.chAge.Width = 100;
+            // 
+            // chWork
+            // 
+            this.chWork.Width = 150;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 303);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtWork);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblWork);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lvView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.Text = "입력목록보기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvView;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblWork;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtWork;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chAge;
+        private System.Windows.Forms.ColumnHeader chWork;
+    }
+}
+
---
+++ B-2/B_ListView/Form2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B_ListView
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        string strName, strAge, strWork;
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (TextCheck())
+            {
+                this.strName = txtName.Text;
+                this.strAge = txtAge.Text;
+                this.strWork = txtWork.Text;
+
+                txtName.Text = "";
+                txtAge.Text = "";
+                txtWork.Text = "";
+            }
+            else return;
+            ListViewItem lvi = new ListViewItem(new string[] { strName, strAge, strWork });
+            this.lvView.Items.Add(lvi);
+        }
+
+        private void txtWork_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)13)
+            {
+                e.Handled = true;
+                this.btnAdd_Click(sender, e);
+            }
+        }
+
+        private bool TextCheck()
+        {
+            if(this.txtName.Text != "" && this.txtAge.Text != "" && this.txtWork.Text != "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void lvView_Click(object sender, EventArgs e)
+        {
+            if (lvView.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("아이템을 선택하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                string strInfo = "이름 : " + lvView.SelectedItems[0].SubItems[0].Text +
+                    "\n나이 : " + lvView.SelectedItems[0].SubItems[1].Text +
+                    "\n직업 : " + lvView.SelectedItems[0].SubItems[2].Text;
+                MessageBox.Show(strInfo, "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
