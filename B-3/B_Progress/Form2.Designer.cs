9c775335e536b3b65a49e45f33cfd685eab94a52
Enterprise09 <goldenstaytes@gmail.com>
2021-09-14 14:35:55 +0900

    update design

--- B-3/B_Progress/Form2.Designer.cs
+++ B-3/B_Progress/Form2.Designer.cs
@@ -40,14 +40,14 @@ namespace B_Progress
             // 
             this.pbStaus.Location = new System.Drawing.Point(0, 0);
             this.pbStaus.Name = "pbStaus";
-            this.pbStaus.Size = new System.Drawing.Size(269, 95);
+            this.pbStaus.Size = new System.Drawing.Size(269, 36);
             this.pbStaus.TabIndex = 0;
             // 
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
             this.lblStatus.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblStatus.Location = new System.Drawing.Point(12, 101);
+            this.lblStatus.Location = new System.Drawing.Point(12, 53);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(39, 20);
             this.lblStatus.TabIndex = 1;
@@ -55,7 +55,7 @@ namespace B_Progress
             // 
             // btnRun
             // 
-            this.btnRun.Location = new System.Drawing.Point(194, 101);
+            this.btnRun.Location = new System.Drawing.Point(194, 53);
             this.btnRun.Name = "btnRun";
             this.btnRun.Size = new System.Drawing.Size(75, 23);
             this.btnRun.TabIndex = 2;
@@ -72,7 +72,7 @@ namespace B_Progress
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(273, 85);
             this.Controls.Add(this.btnRun);
             this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.pbStaus);
