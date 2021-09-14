a981b59c15e8212683e106b49d868a0ce9c73404
Enterprise09 <goldenstaytes@gmail.com>
2021-09-14 14:33:28 +0900

    create progress project & complete

---
+++ B-3/B_Progress/Form2.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace B_Progress
+{
+    partial class Form2
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbStaus = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pbStaus
+            // 
+            this.pbStaus.Location = new System.Drawing.Point(0, 0);
+            this.pbStaus.Name = "pbStaus";
+            this.pbStaus.Size = new System.Drawing.Size(269, 95);
+            this.pbStaus.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblStatus.Location = new System.Drawing.Point(12, 101);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(39, 20);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "상태";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(194, 101);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "진행";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pbStaus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.Text = "상태 진행";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pbStaus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ B-3/B_Progress/Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B_Progress
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        int Num = 0;
+        string OrgStr = "";
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = true;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            OrgStr = this.lblStatus.Text;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Num++;
+            if(Num > 100)
+            {
+                this.Timer.Enabled = false;
+                return;
+            }
+            this.pbStaus.Value = Num;
+            this.lblStatus.Text = OrgStr + " : " + Num.ToString() + "%";
+        }
+    }
+}
