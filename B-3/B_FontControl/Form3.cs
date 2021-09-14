d0226e0b36aa6aaaaf29d3dd645b0e2919b006e5
Enterprise09 <goldenstaytes@gmail.com>
2021-09-14 14:49:21 +0900

    complete font-control project

---
+++ B-3/B_FontControl/Form3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B_FontControl
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void tsBtnFont_Click(object sender, EventArgs e)
+        {
+            if(this.fontDlg.ShowDialog() == DialogResult.OK)
+            {
+                this.rtbText.SelectionFont = this.fontDlg.Font;
+            }
+        }
+
+        private void tsBtnColor_Click(object sender, EventArgs e)
+        {
+            if(this.colorDlg.ShowDialog() == DialogResult.OK)
+            {
+                this.rtbText.SelectionColor = this.colorDlg.Color;
+            }
+        }
+    }
+}
