7a5ba7e380adbc1d82ba09518d61409f09fe1a79
Enterprise09 <goldenstaytes@gmail.com>
2021-09-14 14:20:55 +0900

    complete timer project

--- B-3/B_Timer/Form1.Designer.cs
+++ B-3/B_Timer/Form1.Designer.cs
@@ -30,12 +30,65 @@ namespace B_Timer
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtCountDown = new System.Windows.Forms.TextBox();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(12, 12);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 23);
+            this.txtNum.TabIndex = 0;
+            // 
+            // txtCountDown
+            // 
+            this.txtCountDown.Location = new System.Drawing.Point(12, 50);
+            this.txtCountDown.Name = "txtCountDown";
+            this.txtCountDown.ReadOnly = true;
+            this.txtCountDown.Size = new System.Drawing.Size(100, 23);
+            this.txtCountDown.TabIndex = 1;
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(118, 50);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(93, 23);
+            this.btnCount.TabIndex = 2;
+            this.btnCount.Text = "카운트 다운";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(228, 86);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.txtCountDown);
+            this.Controls.Add(this.txtNum);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "타이머";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtCountDown;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Timer Timer;
     }
 }
 
--- B-3/B_Timer/Form1.cs
+++ B-3/B_Timer/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace B_Timer
 {
@@ -17,5 +11,44 @@ namespace B_Timer
             InitializeComponent();
         }
 
+        int CountOrgNum = 0;
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false;
+                this.txtNum.ReadOnly = false;
+                this.txtNum.Text = "";
+                this.txtCountDown.Text = "";
+                MessageBox.Show("펑", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                this.txtCountDown.Text = Convert.ToString(CountOrgNum);
+                CountOrgNum--;                
+            }
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            if (intCheck())
+            {
+                CountOrgNum = Convert.ToInt32(this.txtNum.Text);
+                this.txtNum.ReadOnly = true;
+                this.Timer.Enabled = true;
+            }
+        }
+
+        private bool intCheck()
+        {
+            if (Information.IsNumeric(this.txtNum.Text))
+                return true;
+            else
+            {
+                MessageBox.Show("숫자를 입력하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
     }
 }
