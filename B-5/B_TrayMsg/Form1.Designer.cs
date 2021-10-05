769f6225947b24c4bc71cd044cae883de7a9279c
Enterprise09 <goldenstaytes@gmail.com>
2021-10-05 14:11:45 +0900

    [+] form2 & design

--- B-5/B_TrayMsg/Form1.Designer.cs
+++ B-5/B_TrayMsg/Form1.Designer.cs
@@ -29,13 +29,47 @@ namespace B_TrayMsg
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnMsg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(0, 0);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(100, 23);
+            this.txtMsg.TabIndex = 0;
+            // 
+            // btnMsg
+            // 
+            this.btnMsg.Location = new System.Drawing.Point(12, 29);
+            this.btnMsg.Name = "btnMsg";
+            this.btnMsg.Size = new System.Drawing.Size(75, 23);
+            this.btnMsg.TabIndex = 1;
+            this.btnMsg.Text = "보이기";
+            this.btnMsg.UseVisualStyleBackColor = true;
+            this.btnMsg.Click += new System.EventHandler(this.btnMsg_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnMsg);
+            this.Controls.Add(this.txtMsg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "트레이 메시지";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnMsg;
     }
 }
 
