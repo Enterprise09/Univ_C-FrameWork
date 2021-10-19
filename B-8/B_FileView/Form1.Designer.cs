a336df84bc520a687111c92e4583f680e968310f
Enterprise09 <goldenstaytes@gmail.com>
2021-10-19 15:51:41 +0900

    complete FileView project

---
+++ B-8/B_FileView/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace B_FileView
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPath = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.rbtnAll = new System.Windows.Forms.RadioButton();
+            this.rbtnHidden = new System.Windows.Forms.RadioButton();
+            this.lvFile = new System.Windows.Forms.ListView();
+            this.fbdFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.ssBar = new System.Windows.Forms.StatusStrip();
+            this.chFilePath = new System.Windows.Forms.ColumnHeader();
+            this.chFileName = new System.Windows.Forms.ColumnHeader();
+            this.chFileTime = new System.Windows.Forms.ColumnHeader();
+            this.chFileSize = new System.Windows.Forms.ColumnHeader();
+            this.tsslblResult = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ssBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPath.Location = new System.Drawing.Point(12, 10);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(58, 21);
+            this.lblPath.TabIndex = 0;
+            this.lblPath.Text = "경로 : ";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(76, 10);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(712, 23);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(323, 49);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(117, 23);
+            this.btnPath.TabIndex = 2;
+            this.btnPath.Text = "경로";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // rbtnAll
+            // 
+            this.rbtnAll.AutoSize = true;
+            this.rbtnAll.Location = new System.Drawing.Point(76, 51);
+            this.rbtnAll.Name = "rbtnAll";
+            this.rbtnAll.Size = new System.Drawing.Size(77, 19);
+            this.rbtnAll.TabIndex = 3;
+            this.rbtnAll.TabStop = true;
+            this.rbtnAll.Text = "전체 파일";
+            this.rbtnAll.UseVisualStyleBackColor = true;
+            this.rbtnAll.CheckedChanged += new System.EventHandler(this.rbtnAll_CheckedChanged);
+            // 
+            // rbtnHidden
+            // 
+            this.rbtnHidden.AutoSize = true;
+            this.rbtnHidden.Location = new System.Drawing.Point(171, 51);
+            this.rbtnHidden.Name = "rbtnHidden";
+            this.rbtnHidden.Size = new System.Drawing.Size(77, 19);
+            this.rbtnHidden.TabIndex = 4;
+            this.rbtnHidden.TabStop = true;
+            this.rbtnHidden.Text = "숨김 파일";
+            this.rbtnHidden.UseVisualStyleBackColor = true;
+            this.rbtnHidden.CheckedChanged += new System.EventHandler(this.rbtnHidden_CheckedChanged);
+            // 
+            // lvFile
+            // 
+            this.lvFile.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chFilePath,
+            this.chFileName,
+            this.chFileTime,
+            this.chFileSize});
+            this.lvFile.GridLines = true;
+            this.lvFile.HideSelection = false;
+            this.lvFile.Location = new System.Drawing.Point(0, 99);
+            this.lvFile.Name = "lvFile";
+            this.lvFile.Size = new System.Drawing.Size(800, 351);
+            this.lvFile.TabIndex = 5;
+            this.lvFile.UseCompatibleStateImageBehavior = false;
+            this.lvFile.View = System.Windows.Forms.View.Details;
+            // 
+            // ssBar
+            // 
+            this.ssBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslblResult});
+            this.ssBar.Location = new System.Drawing.Point(0, 428);
+            this.ssBar.Name = "ssBar";
+            this.ssBar.Size = new System.Drawing.Size(800, 22);
+            this.ssBar.TabIndex = 6;
+            this.ssBar.Text = "statusStrip1";
+            // 
+            // chFilePath
+            // 
+            this.chFilePath.Text = "경로";
+            this.chFilePath.Width = 400;
+            // 
+            // chFileName
+            // 
+            this.chFileName.Text = "이름";
+            this.chFileName.Width = 120;
+            // 
+            // chFileTime
+            // 
+            this.chFileTime.Text = "수정한 날짜";
+            this.chFileTime.Width = 150;
+            // 
+            // chFileSize
+            // 
+            this.chFileSize.Text = "크기";
+            this.chFileSize.Width = 150;
+            // 
+            // tsslblResult
+            // 
+            this.tsslblResult.Name = "tsslblResult";
+            this.tsslblResult.Size = new System.Drawing.Size(122, 17);
+            this.tsslblResult.Text = "폴더 : 0개, 파일 : 0개";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ssBar);
+            this.Controls.Add(this.lvFile);
+            this.Controls.Add(this.rbtnHidden);
+            this.Controls.Add(this.rbtnAll);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "파일 보기";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ssBar.ResumeLayout(false);
+            this.ssBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.RadioButton rbtnAll;
+        private System.Windows.Forms.RadioButton rbtnHidden;
+        private System.Windows.Forms.ListView lvFile;
+        private System.Windows.Forms.ColumnHeader chFilePath;
+        private System.Windows.Forms.ColumnHeader chFileName;
+        private System.Windows.Forms.ColumnHeader chFileTime;
+        private System.Windows.Forms.ColumnHeader chFileSize;
+        private System.Windows.Forms.FolderBrowserDialog fbdFolder;
+        private System.Windows.Forms.StatusStrip ssBar;
+        private System.Windows.Forms.ToolStripStatusLabel tsslblResult;
+    }
+}
+
