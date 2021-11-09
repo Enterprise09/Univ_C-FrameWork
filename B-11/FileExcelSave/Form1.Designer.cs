292738881f1e55c6402705f4e73cbf81a914fa3e
Enterprise09 <goldenstaytes@gmail.com>
2021-11-09 14:28:53 +0900

    [+] form design

--- B-11/FileExcelSave/Form1.Designer.cs
+++ B-11/FileExcelSave/Form1.Designer.cs
@@ -29,13 +29,154 @@ namespace FileExcelSave
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.gbSave = new System.Windows.Forms.GroupBox();
+            this.rbText = new System.Windows.Forms.RadioButton();
+            this.rbExcel = new System.Windows.Forms.RadioButton();
+            this.lvFile = new System.Windows.Forms.ListView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.chName = new System.Windows.Forms.ColumnHeader();
+            this.chDate = new System.Windows.Forms.ColumnHeader();
+            this.chType = new System.Windows.Forms.ColumnHeader();
+            this.chSize = new System.Windows.Forms.ColumnHeader();
+            this.chPath = new System.Windows.Forms.ColumnHeader();
+            this.gbSave.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(12, 22);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(90, 48);
+            this.btnPath.TabIndex = 0;
+            this.btnPath.Text = "파일";
+            this.btnPath.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(189, 9);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 50);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // gbSave
+            // 
+            this.gbSave.Controls.Add(this.rbExcel);
+            this.gbSave.Controls.Add(this.rbText);
+            this.gbSave.Controls.Add(this.btnSave);
+            this.gbSave.Location = new System.Drawing.Point(108, 12);
+            this.gbSave.Name = "gbSave";
+            this.gbSave.Size = new System.Drawing.Size(287, 62);
+            this.gbSave.TabIndex = 2;
+            this.gbSave.TabStop = false;
+            this.gbSave.Text = "파일 저장";
+            // 
+            // rbText
+            // 
+            this.rbText.AutoSize = true;
+            this.rbText.Location = new System.Drawing.Point(24, 26);
+            this.rbText.Name = "rbText";
+            this.rbText.Size = new System.Drawing.Size(44, 19);
+            this.rbText.TabIndex = 0;
+            this.rbText.TabStop = true;
+            this.rbText.Text = "TXT";
+            this.rbText.UseVisualStyleBackColor = true;
+            // 
+            // rbExcel
+            // 
+            this.rbExcel.AutoSize = true;
+            this.rbExcel.Location = new System.Drawing.Point(90, 26);
+            this.rbExcel.Name = "rbExcel";
+            this.rbExcel.Size = new System.Drawing.Size(58, 19);
+            this.rbExcel.TabIndex = 1;
+            this.rbExcel.TabStop = true;
+            this.rbExcel.Text = "EXCEL";
+            this.rbExcel.UseVisualStyleBackColor = true;
+            // 
+            // lvFile
+            // 
+            this.lvFile.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chDate,
+            this.chType,
+            this.chSize,
+            this.chPath});
+            this.lvFile.FullRowSelect = true;
+            this.lvFile.GridLines = true;
+            this.lvFile.HideSelection = false;
+            this.lvFile.Location = new System.Drawing.Point(2, 80);
+            this.lvFile.Name = "lvFile";
+            this.lvFile.Size = new System.Drawing.Size(796, 371);
+            this.lvFile.TabIndex = 3;
+            this.lvFile.UseCompatibleStateImageBehavior = false;
+            this.lvFile.View = System.Windows.Forms.View.Details;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // chName
+            // 
+            this.chName.Text = "이름";
+            this.chName.Width = 150;
+            // 
+            // chDate
+            // 
+            this.chDate.Text = "수정한 날짜";
+            this.chDate.Width = 100;
+            // 
+            // chType
+            // 
+            this.chType.Text = "유형";
+            this.chType.Width = 100;
+            // 
+            // chSize
+            // 
+            this.chSize.Text = "크기";
+            this.chSize.Width = 100;
+            // 
+            // chPath
+            // 
+            this.chPath.Text = "경로";
+            this.chPath.Width = 250;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lvFile);
+            this.Controls.Add(this.gbSave);
+            this.Controls.Add(this.btnPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "파일 정보 저장하기";
+            this.gbSave.ResumeLayout(false);
+            this.gbSave.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox gbSave;
+        private System.Windows.Forms.RadioButton rbExcel;
+        private System.Windows.Forms.RadioButton rbText;
+        private System.Windows.Forms.ListView lvFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chDate;
+        private System.Windows.Forms.ColumnHeader chType;
+        private System.Windows.Forms.ColumnHeader chSize;
+        private System.Windows.Forms.ColumnHeader chPath;
     }
 }
 
